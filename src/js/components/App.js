116a2d39fd6e6ac3dd7807c2419604554956d667
Timothy Gerstel <tgerstel@umass.edu>
2019-01-28 00:26:26 -0500

    Converting project to webpack+react application
    Signed-off-by: Timothy Gerstel <tgerstel@umass.edu>

---
+++ src/js/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+window.onload = function(){
+  return console.log('Loaded app');
+};
+
+class App extends React.Component {
+  render(){
+    return <div><h1>Fingerprinter App</h1></div>;
+  }
+}
+
+export default App;
